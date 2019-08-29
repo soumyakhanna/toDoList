da0b94219af817e65c66ec81e36acc5b08447dca
Soumya Khanna <soumya.khanna99@gmail.com>
2019-08-28 20:04:03 -0400

    Add files via upload

---
+++ toDoList.js
@@ -0,0 +1,30 @@
+var theme = document.querySelector(".theme");
+
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+     event.stopPropagation();
+});
+
+$("input[type ='text']").keypress(function(event){
+  if(event.which === 13){
+      var toDoText = $(this).val();
+      $(this).val("");
+      $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + toDoText + "</li");
+  }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+})
+
+function setupThemes(){
+    theme.addEventListener("click", function(){
+    
+    });
+}
